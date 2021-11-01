530f6e285c1534c411cbbec88b1e020cc7d9c79d
Arda098 <matris@hotmail.com.au>
2021-11-01 19:30:58 +1100

    changes uploaded

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
 var platform
+var log6
+var chain
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,7 +39,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(200,100,80, PI/2);
 
+    chain = new Chain(bird.body, log6.body);
 }
 
 function draw(){
@@ -62,4 +67,8 @@ function draw(){
     log5.display();
 
     bird.display();
+    log6.display();
+
+    chain.display();
+    
 }
